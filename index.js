d378e84ecceccc8dc997668e7db63b4808f44ab8
stanley92 <Stanley92@hotmail.com>
2021-10-18 16:11:00 +0800

    Create Assignments

---
+++ index.js
@@ -0,0 +1,17 @@
+const insertionSort = require("./insertion-sort");
+const selectionSort = require("./selection-sort");
+const bubbleSort = require("./bubble-sort");
+
+const numberArray = Array.from({ length: 10 }, () =>
+  Math.floor(Math.random() * 100)
+);
+console.log("Checking numberArray:", numberArray);
+
+const insertionSortedArray = insertionSort(numberArray);
+console.log("Checking insertionSortedArray:", insertionSortedArray);
+
+const selectionSortedArray = selectionSort(numberArray);
+console.log("Checking selectionSortedArray:", selectionSortedArray);
+
+const bubbleSortedArray = bubbleSort(numberArray);
+console.log("Checking bubbleSortedArray:", bubbleSortedArray);
---
+++ insertion-sort.js
@@ -0,0 +1,16 @@
+// Task 1: To implement insertion sort and return a new sorted array as the result without modifying the array parameter.
+
+//Task 2: To determine the 3 cases of Big O Notation
+
+function insertionSort(array) {
+  //implement code here
+  let toBeSortedArray = [];
+
+  return toBeSortedArray;
+}
+// Big O Notation
+// Worse Case: ?
+// Average Case: ?
+// Best Case: ?
+
+module.exports = insertionSort;
